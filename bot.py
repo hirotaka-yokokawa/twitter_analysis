710f567ed0e461ba0b2f886ecfe3456839d99476
hibikikkk <kyou9712@gmail.com>
2018-12-06 14:47:27 +0900

    herokuの環境が変えたので、procfileの呼び出し方で3をつけるようにした

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ def tweet(index):
 
     options = Options()
     options.set_headless(Options.headless)
-    driver = webdriver.PhantomJS(desired_capabilities=dcap, executable_path="./phantomjs-2.1.1-macosx/bin/phantomjs")
+    driver = webdriver.PhantomJS(desired_capabilities=dcap)
     driver.set_window_size(1124, 850)
     driver.get("https://twitter.com/")
     driver.find_element_by_name("session[username_or_email]").send_keys("hibikikkk_9712")
